dc453aeb4c1da7b70e0498c0c56d28e4bf87d0f8
RenaudBernier <renaudbernier@hotmail.fr>
2025-01-30 22:06:52 -0500

    Added PrismaAdapter

--- team3/app/api/auth/[...nextauth]/route.ts
+++ team3/app/api/auth/[...nextauth]/route.ts
@@ -31,7 +31,7 @@ const authOptions: NextAuthOptions = {
         signOut: '/auth/signout',
         error: '/auth/error', // Error code passed in query string as ?error=
         verifyRequest: '/auth/verify-request', // (used for check email message)
-        newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+        newUser: '/auth/new-user' // New users will be directed here on first sign in
     }
 }
 const handler = NextAuth(authOptions);
