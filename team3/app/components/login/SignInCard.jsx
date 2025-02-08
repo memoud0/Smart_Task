81b2ab98f258d6df330eb4ed5368bb4f31eb9d8c
M-a-a-d-man <evanteboul@icloud.com>
2025-02-08 18:31:26 -0500

    created landing page/signin page

---
+++ team3/app/components/login/SignInCard.jsx
@@ -0,0 +1,151 @@
+"use client";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+// import Card from '@mui/material/Card';
+import MuiCard from '@mui/material/Card';
+// import { useTheme } from 'next-themes';
+import * as React from 'react';
+// import { FacebookIcon, GoogleIcon, SitemarkIcon } from './CustomIcons';
+// import ForgotPassword from './ForgotPassword';
+
+const Card = styled(MuiCard)(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+  alignItems: 'center',
+  width: '100%',
+  padding: '4px',
+  gap: '2px',
+  margin: 'auto',
+  boxShadow:
+    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+  backgroundColor: '#00000',
+  color: '#000000',
+}));
+
+
+export default function SignInCard({ onToggle, handleSubmit }) {  
+  const [emailError, setEmailError] = React.useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = React.useState('');
+  const [passwordError, setPasswordError] = React.useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState('');
+
+
+
+  const validateInputs = () => {
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+
+    let isValid = true;
+
+    if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+      setEmailError(true);
+      setEmailErrorMessage('Please enter a valid email address.');
+      isValid = false;
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage('');
+    }
+
+    if (!password.value || password.value.length < 6) {
+      setPasswordError(true);
+      setPasswordErrorMessage('Password must be at least 6 characters long.');
+      isValid = false;
+    } else {
+      setPasswordError(false);
+      setPasswordErrorMessage('');
+    }
+
+    return isValid;
+  };
+
+  
+
+  return (
+    
+    <Card variant="outlined">
+      {/* <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+        <SitemarkIcon />
+      </Box> */}
+      <Typography
+        component="h1"
+        variant="h4"
+        sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+      >
+        Sign in
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        noValidate
+        sx={{ display: 'flex', flexDirection: 'column', width: '100%', gap: 2 }}
+      >
+        <FormControl>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <TextField
+            error={emailError}
+            helperText={emailErrorMessage}
+            id="email"
+            type="email"
+            name="email"
+            placeholder="your@email.com"
+            autoComplete="email"
+            autoFocus
+            required
+            fullWidth
+            variant="outlined"
+            color={emailError ? 'error' : 'primary'}
+          />
+        </FormControl>
+        <FormControl>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <FormLabel htmlFor="password">Password</FormLabel>
+          </Box>
+          <TextField
+            error={passwordError}
+            helperText={passwordErrorMessage}
+            name="password"
+            placeholder="••••••"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            required
+            fullWidth
+            variant="outlined"
+            color={passwordError ? 'error' : 'primary'}
+          />
+        </FormControl>
+        
+        <Button type="submit" fullWidth variant="contained" onSubmit={()=>{
+          if(validateInputs()){
+            handleSubmit();
+          }
+          
+        }}>
+          Sign in
+        </Button>
+        <Typography sx={{ textAlign: 'center' }}>
+          Don&apos;t have an account?{' '}
+          <Link
+            href="#"
+            variant="body2"
+            onClick={onToggle}
+            sx={{ alignSelf: 'center', cursor: 'pointer' }}
+          >
+            Sign up
+          </Link>
+        </Typography>
+      </Box>
+    </Card>
+    
+  );
+  
+}
