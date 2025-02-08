e84ae82ed97f5d8218478ba0a3b4127196138a8a
RenaudBernier <renaudbernier@hotmail.fr>
2025-02-08 17:29:46 -0500

    Prisma fix

--- team3/app/api/auth/[...nextauth]/route.ts
+++ team3/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import type { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import {PrismaClient} from "@prisma/client/extension";
+import {PrismaClient} from "@prisma/client";
 
 const prisma = new PrismaClient();
 
