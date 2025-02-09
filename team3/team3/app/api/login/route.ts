2427c485602d0b7b3197ccf42160c1a2670096d5
Kenny <54686876+Kenny-star@users.noreply.github.com>
2025-02-09 14:09:57 -0500

    push

---
+++ team3/team3/app/api/login/route.ts
@@ -0,0 +1,3 @@
+import { LocaleRouteNormalizer } from "next/dist/server/normalizers/locale-route-normalizer";
+
+// fetch("localhost:3000/api/scan", {)
---
+++ team3/team3/app/components/Calendar/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+// import Image from "next/image";
+import DemoApp from "./demoApp";
+// import Calendar from "./components/calendar";
+
+export default function Home() {
+  return (
+    <>
+    <DemoApp />
+    {/* <Calendar /> */}
+    </>
+      
+  );
+}
